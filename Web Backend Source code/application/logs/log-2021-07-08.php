6378781cc648fba936d745ea81ce95d40075a0c6
Aririzqi13 <aririzqi13@gmail.com>
2021-07-08 21:44:16 +0700

    0.1

---
+++ Web Backend Source code/application/logs/log-2021-07-08.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-07-08 06:05:59 --> 404 Page Not Found: Uas_ws/Web%20Backend%20Source%20code
+ERROR - 2021-07-08 16:43:40 --> 404 Page Not Found: Uas_ws/Web%20Backend%20Source%20code
